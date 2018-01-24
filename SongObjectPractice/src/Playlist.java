556d4344d6c4641a6e45fd07cd35441ea990cfb3
owen.halbrook <owen.halbrook@MU-C1-12.mullen.pvt.k12.co.us>
2018-01-24 10:18:45 -0700

    Finished playlist

--- SongObjectPractice/src/Playlist.java
+++ SongObjectPractice/src/Playlist.java
@@ -8,18 +8,27 @@ public class Playlist
 
 				fillArray();
 				printList();
-				playlistEdit();
+				playlistEdit1();
+				printList();
+				playlistEdit2();
+				printList();
+				playlistEdit3();
+				
 			}
+
+
+		
+
 
 
 		private static void fillArray()
 			{
 
-				playlist.add(new Song("Paddy's Meme Track", "BrokeBoy Records", 3600));
-				playlist.add(new Song("Gassed up Paddy", "BrokeBoy Records", 520));
-				playlist.add(new Song("School", "BrokeBoy Records", 9000000));
-				playlist.add(new Song("Keaton's beeen dissssssed", "Luke,James,Zach", 3));
-				playlist.add(new Song("Air.............Pods", "PaddyBoy Records", 4630));
+				playlist.add(new Song(" Paddy's Meme Track ", "BrokeBoy Records ", 3600));
+				playlist.add(new Song(" Gassed up Paddy ", "BrokeBoy Records ", 520));
+				playlist.add(new Song(" School ", "BrokeBoy Records ", 9000000));
+				playlist.add(new Song(" Keaton's beeen dissssssed ", "Luke,James,Zach ", 3));
+				playlist.add(new Song(" Air.............Pods ", "PaddyBoy Records ", 4630));
 				
 			}
 
@@ -29,15 +38,45 @@ public class Playlist
 					{
 						System.out.println(playlist.get(i).getTitle() + playlist.get(i).getArtist() + playlist.get(i).getLengthInSeconds());
 					}
-				
+				System.out.println(" ");
 			}
 
-		private static void playlistEdit()
+		private static void playlistEdit1()
 			{
 				for(int i = 0; i < playlist.size(); i++)
 					{
-						playlist.get(i).setLengthInSeconds(playlist.get(i).getLengthInSeconds - 10);
+						playlist.get(i).setLengthInSeconds(playlist.get(i).getLengthInSeconds() - 10);
 					}
 				
 			}
+		private static void playlistEdit2()
+			{
+				int max = 0;
+				int length = playlist.get(0).getTitle().length();
+				for(int i = 0; i < playlist.size(); i++)
+					{
+						if(length > max)
+							{
+								max = length;
+							}
+					}
+				for(int u = 0; u < playlist.size(); u++)
+					{
+						if(playlist.get(u).getTitle().length() == max)
+							{
+								playlist.remove(u);
+							}
+					}
+				
+				
+			}
+
+		private static void playlistEdit3()
+			{
+				playlist.get(0).setArtist("Owen Halbrook");
+				
+			}
+
+
+
 	}
